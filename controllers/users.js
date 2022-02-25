e9b36282cd025aadb56fd76328234d14943c62b1
Kudzai <52626263+kculz@users.noreply.github.com>
2022-02-25 10:21:07 +0200

    white space removed

--- controllers/users.js
+++ controllers/users.js
@@ -1,34 +1,27 @@
 import {v4 as uuidv4} from 'uuid'
 let users =[]
-
 export const getUsers =  (req,res)=>{
     res.send(users)
   }
-
 export const createUser = (req,res)=>{
     const newUser = req.body
     users.push({...newUser,id:uuidv4()})
    res.send(`User with name ${newUser.firstname} is added to database`)
  }
-
 export const getUser = (req,res)=>{
    const {id} = req.params
    const foundUser = users.find((user) => user.id === id)
    res.send(foundUser)
   }
-
 export const deleteUser = (req,res)=>{
     const {id} = req.params
     users = users.filter((user)=> user.id !== id)
     res.send(`User with ID# ${id} is successfully removed from the database`)
   }
-
 export const updateUser = (req,res)=>{ 
     const {id} = req.params
     const { firstname, lastname, age } = req.body
-
     const user = users.find((user) => user.id === id)
-
     if(firstname) user.firstname = firstname
     if(lastname) user.lastname = lastname
     if(age) user.age = age
